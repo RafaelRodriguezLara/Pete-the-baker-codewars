0360a3ec088e4fdea3eabf82b6b9ac091d69cc6d
Rafael1548 <35503348+Rafael1548@users.noreply.github.com>
2019-07-30 17:04:20 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,5 @@
 import math
 
-recipe = {"flour": 500, "sugar": 200, "eggs": 1}
-available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
 
 def cakes(recipe, available):
     ingredient_amounts=[]
