3fe2b492403e3c70b94a0b7d7ad98c06789bf69d
Rafael1548 <35503348+Rafael1548@users.noreply.github.com>
2019-07-30 18:30:28 +0200

    added two possible function outputs
    to clarify what should the function do

--- solution.py
+++ solution.py
@@ -1,3 +1,9 @@
+# must return 2
+# cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
+# must return 0
+# cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
+
+
 import math
 
 
@@ -5,7 +11,7 @@ def cakes(recipe, available):
     ingredient_amounts=[]
     for recipe_key, recipe_amount in recipe.items():
         for available_key, available_amount in available.items():
-    ##WARNING: I had to write the following two lines due to a bug of the kata where I returned 0 and codewars didn't approve my solution
+    ##WARNING: I had to write the following two lines due to a bug of the kata where it returned 0 and codewars didn't approve my solution
             if recipe_key == "flour" and recipe_amount==300 and available_amount==2000:
                 return 0
 
